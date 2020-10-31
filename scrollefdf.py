ea86394e0fbac2c7b909b2c154fd9cd87f8b2dab
yanmyo309 <yanmyo3092@gmail.com>
2020-10-31 11:21:54 +0630

    My First Commit

---
+++ scrollefdf.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import scrolledtext
+import tkinter as tk
+
+root =Tk()
+text =scrolledtext.ScrolledText(root,width =30,height=30,wrap=tk.WORD).pack()
+
+
+
+
+root.mainloop()
